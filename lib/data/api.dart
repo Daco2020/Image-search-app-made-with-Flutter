dd18202067059419a90df88b092250dfe35d1c93
Daco2020 <dacokim32@gmail.com>
2022-12-06 23:17:48 +0900

    refactor: 기능 분리하기

---
+++ lib/data/api.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:image_search/model/Photo.dart';
+
+class PixabayApi {
+  final baseUrl = 'https://pixabay.com/api/';
+  final key = '31754924-45f867a911fee356971928f35';
+
+  Future<List<Photo>> fetch(String query) async {
+    final response = await http.get(Uri.parse(
+        '$baseUrl?key=$key&q=$query&image_type=photo'));
+
+    Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+    Iterable hits = jsonResponse['hits'];
+    return hits.map((e) => Photo.fromJson(e)).toList();
+  }
+}
